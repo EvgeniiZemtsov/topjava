345c6d287e7b3b2672e78c62e766c0306eb5e421
EugeneZe <sever-zema@mail.ru>
2021-10-20 20:28:57 +0300

    HW01

--- src/main/java/ru/javawebinar/topjava/web/MealsServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealsServlet.java
@@ -1,5 +1,8 @@
 package ru.javawebinar.topjava.web;
 
+import jdk.internal.logger.LoggerWrapper;
+import ru.javawebinar.topjava.util.UserMealsUtil;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,9 +10,12 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class MealsServlet extends HttpServlet {
+    private static final LoggerWrapper log = LoggerWrapper.get(MealsServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.sendRedirect("meals.jsp");
+        log.info("getAll");
+        req.setAttribute("mealList", UserMealsUtil.getFilteredWithExceeded(UserMealsUtil.MEAL_LIST, 2000));
+        resp.getRequestDispatcher("mealList.jsp").forward(req, resp);
     }
 }
